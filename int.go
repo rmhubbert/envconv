c27fe33e165652d36ecc8775da8e0e09786d68d4
Hubby <hubby@afterdigital.co.uk>
2024-06-21 14:29:58 +0100

    Add int8, int16, int32 and int64 conversion functions and tests

--- int.go
+++ int.go
@@ -4,12 +4,12 @@ import "strconv"
 
 // convertInt converts the passed string to an int. It will
 // also return an error, if applicable.
-func convertInt(value string) (int, error) {
-	convertedValue, err := strconv.Atoi(value)
+func convertInt(value string, bitSize int) (int, error) {
+	convertedValue, err := strconv.ParseInt(value, 10, bitSize)
 	if err != nil {
-		return 0, err
+		return int(0), err
 	}
-	return convertedValue, nil
+	return int(convertedValue), nil
 }
 
 // ToInt returns the value of the requested environment variable
@@ -22,7 +22,7 @@ func ToInt(varName string) (int, error) {
 		return 0, err
 	}
 
-	return convertInt(value)
+	return convertInt(value, 64)
 }
 
 // ToIntWithDefault returns the value of the requested environment
@@ -36,9 +36,153 @@ func ToIntWithDefault(varName string, defaultValue int) int {
 	}
 
 	var convertedValue int
-	convertedValue, err = convertInt(value)
+	convertedValue, err = convertInt(value, 64)
 	if err != nil {
 		return defaultValue
 	}
 	return convertedValue
+}
+
+// ToInt8 returns the value of the requested environment variable
+// converted to an int8. An error will be returned if the
+// environment variable is not found or the conversion to
+// int8 fails.
+func ToInt8(varName string) (int8, error) {
+	value, err := LoadFromEnvironment(varName, true)
+	if err != nil {
+		return 0, err
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 8)
+	if err != nil {
+		return 0, err
+	}
+	return int8(convertedValue), nil
+}
+
+// ToInt8WithDefault returns the value of the requested environment
+// variable converted to an int8. The default value passed as
+// the second parameter will be returned if the environment
+// variable is not found or the conversion to int8 fails.
+func ToInt8WithDefault(varName string, defaultValue int8) int8 {
+	value, err := LoadFromEnvironment(varName, false)
+	if err != nil {
+		return defaultValue
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 8)
+	if err != nil {
+		return defaultValue
+	}
+	return int8(convertedValue)
+}
+
+// ToInt16 returns the value of the requested environment variable
+// converted to an int16. An error will be returned if the
+// environment variable is not found or the conversion to
+// int16 fails.
+func ToInt16(varName string) (int16, error) {
+	value, err := LoadFromEnvironment(varName, true)
+	if err != nil {
+		return 0, err
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 16)
+	if err != nil {
+		return 0, err
+	}
+	return int16(convertedValue), nil
+}
+
+// ToInt16WithDefault returns the value of the requested environment
+// variable converted to an int16. The default value passed as
+// the second parameter will be returned if the environment
+// variable is not found or the conversion to int16 fails.
+func ToInt16WithDefault(varName string, defaultValue int16) int16 {
+	value, err := LoadFromEnvironment(varName, false)
+	if err != nil {
+		return defaultValue
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 16)
+	if err != nil {
+		return defaultValue
+	}
+	return int16(convertedValue)
+}
+
+// ToInt32 returns the value of the requested environment variable
+// converted to an int32. An error will be returned if the
+// environment variable is not found or the conversion to
+// int32 fails.
+func ToInt32(varName string) (int32, error) {
+	value, err := LoadFromEnvironment(varName, true)
+	if err != nil {
+		return 0, err
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 32)
+	if err != nil {
+		return 0, err
+	}
+	return int32(convertedValue), nil
+}
+
+// ToInt32WithDefault returns the value of the requested environment
+// variable converted to an int32. The default value passed as
+// the second parameter will be returned if the environment
+// variable is not found or the conversion to int32 fails.
+func ToInt32WithDefault(varName string, defaultValue int32) int32 {
+	value, err := LoadFromEnvironment(varName, false)
+	if err != nil {
+		return defaultValue
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 32)
+	if err != nil {
+		return defaultValue
+	}
+	return int32(convertedValue)
+}
+
+// ToInt64 returns the value of the requested environment variable
+// converted to an int64. An error will be returned if the
+// environment variable is not found or the conversion to
+// int64 fails.
+func ToInt64(varName string) (int64, error) {
+	value, err := LoadFromEnvironment(varName, true)
+	if err != nil {
+		return 0, err
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int64(convertedValue), nil
+}
+
+// ToInt64WithDefault returns the value of the requested environment
+// variable converted to an int64. The default value passed as
+// the second parameter will be returned if the environment
+// variable is not found or the conversion to int64 fails.
+func ToInt64WithDefault(varName string, defaultValue int64) int64 {
+	value, err := LoadFromEnvironment(varName, false)
+	if err != nil {
+		return defaultValue
+	}
+
+	var convertedValue int
+	convertedValue, err = convertInt(value, 64)
+	if err != nil {
+		return defaultValue
+	}
+	return int64(convertedValue)
 }
--- int_test.go
+++ int_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Tests for int type
 type ToIntTest struct {
 	env         string
 	value       string
@@ -50,24 +51,278 @@ func TestToInt(t *testing.T) {
 	})
 }
 
-func TestToIntWithDefault(t *testing.T) {
-	testData := []ToIntWithDefaultTest{
-		{"TEST_INT_WITH_DEFAULT_105", "105", 105, 10},
-		{"TEST_INT_WITH_DEFAULT_-105", "-105", -105, 11},
-		{"TEST_INT_WITH_DEFAULT_0", "0", 0, 12},
-		{"TEST_INT_WITH_DEFAULT_NOTANUMBER", "notanumber", 13, 13},
+// Tests for int8 type
+type ToInt8Test struct {
+	env         string
+	value       string
+	expected    int8
+	errExpected bool
+}
+
+type ToInt8WithDefaultTest struct {
+	env          string
+	value        string
+	expected     int8
+	defaultValue int8
+}
+
+func TestToInt8(t *testing.T) {
+	testData := []ToInt8Test{
+		{"TEST_INT8_0", "0", 0, false},
+		{"TEST_INT8_127", "127", 127, false},
+		{"TEST_INT8_128", "128", 0, true},
+		{"TEST_INT8_-128", "-128", -128, false},
+		{"TEST_INT8_-129", "-129", 0, true},
+		{"TEST_INT8_NOTANUMBER", "notanumber", 0, true},
 	}
 
 	for _, td := range testData {
 		t.Run(td.env, func(t *testing.T) {
 			os.Setenv(td.env, td.value)
-			v := envconv.ToIntWithDefault(td.env, td.defaultValue)
+			v, err := envconv.ToInt8(td.env)
+			if td.errExpected {
+				assert.Error(t, err, "there should be an error")
+			} else {
+				assert.NoError(t, err, "there should be no error")
+			}
 			assert.Equal(t, td.expected, v, "they should be equal")
 		})
 	}
 
 	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
-		v := envconv.ToIntWithDefault("TEST_NON_EXISTANT", 105)
-		assert.Equal(t, 105, v, "they should be equal")
+		v, err := envconv.ToInt8("TEST_NON_EXISTANT")
+		assert.Error(t, err, "there should be an error")
+		assert.Equal(t, int8(0), v, "they should be equal")
+	})
+}
+
+func TestToInt8WithDefault(t *testing.T) {
+	testData := []ToInt8WithDefaultTest{
+		{"TEST_INT8_WITH_DEFAULT_0", "0", 0, 105},
+		{"TEST_INT8_WITH_DEFAULT_127", "127", 127, 127},
+		{"TEST_INT8_WITH_DEFAULT_128", "128", 0, 0},
+		{"TEST_INT8_WITH_DEFAULT_-128", "-128", -128, 105},
+		{"TEST_INT8_WITH_DEFAULT_-129", "-129", 0, 0},
+		{"TEST_INT8_WITH_DEFAULT_NOTANUMBER", "notanumber", 105, 105},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v := envconv.ToInt8WithDefault(td.env, td.defaultValue)
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v := envconv.ToInt8WithDefault("TEST_NON_EXISTANT", 105)
+		assert.Equal(t, int8(105), v, "they should be equal")
+	})
+}
+
+// Tests for int16 type
+type ToInt16Test struct {
+	env         string
+	value       string
+	expected    int16
+	errExpected bool
+}
+
+type ToInt16WithDefaultTest struct {
+	env          string
+	value        string
+	expected     int16
+	defaultValue int16
+}
+
+func TestToInt16(t *testing.T) {
+	testData := []ToInt16Test{
+		{"TEST_INT16_0", "0", 0, false},
+		{"TEST_INT16_32767", "32767", 32767, false},
+		{"TEST_INT16_32768", "32768", 0, true},
+		{"TEST_INT16_-32768", "-32768", -32768, false},
+		{"TEST_INT16_-32769", "-32769", 0, true},
+		{"TEST_INT16_NOTANUMBER", "notanumber", 0, true},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v, err := envconv.ToInt16(td.env)
+			if td.errExpected {
+				assert.Error(t, err, "there should be an error")
+			} else {
+				assert.NoError(t, err, "there should be no error")
+			}
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v, err := envconv.ToInt16("TEST_NON_EXISTANT")
+		assert.Error(t, err, "there should be an error")
+		assert.Equal(t, int16(0), v, "they should be equal")
+	})
+}
+
+func TestToInt16WithDefault(t *testing.T) {
+	testData := []ToInt16WithDefaultTest{
+		{"TEST_INT16_WITH_DEFAULT_0", "0", 0, 105},
+		{"TEST_INT16_WITH_DEFAULT_32767", "32767", 32767, 32767},
+		{"TEST_INT16_WITH_DEFAULT_32768", "32768", 0, 0},
+		{"TEST_INT16_WITH_DEFAULT_-32768", "-32767", -32767, -32767},
+		{"TEST_INT16_WITH_DEFAULT_-32769", "-32769", 0, 0},
+		{"TEST_INT16_WITH_DEFAULT_NOTANUMBER", "notanumber", 105, 105},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v := envconv.ToInt16WithDefault(td.env, td.defaultValue)
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v := envconv.ToInt16WithDefault("TEST_NON_EXISTANT", 105)
+		assert.Equal(t, int16(105), v, "they should be equal")
+	})
+}
+
+// Tests for int32 type
+type ToInt32Test struct {
+	env         string
+	value       string
+	expected    int32
+	errExpected bool
+}
+
+type ToInt32WithDefaultTest struct {
+	env          string
+	value        string
+	expected     int32
+	defaultValue int32
+}
+
+func TestToInt32(t *testing.T) {
+	testData := []ToInt32Test{
+		{"TEST_INT32_0", "0", 0, false},
+		{"TEST_INT32_2147483647", "2147483647", 2147483647, false},
+		{"TEST_INT32_2147483648", "2147483648", 0, true},
+		{"TEST_INT32_-2147483648", "-2147483648", -2147483648, false},
+		{"TEST_INT32_-2147483649", "-2147483649", 0, true},
+		{"TEST_INT32_NOTANUMBER", "notanumber", 0, true},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v, err := envconv.ToInt32(td.env)
+			if td.errExpected {
+				assert.Error(t, err, "there should be an error")
+			} else {
+				assert.NoError(t, err, "there should be no error")
+			}
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v, err := envconv.ToInt32("TEST_NON_EXISTANT")
+		assert.Error(t, err, "there should be an error")
+		assert.Equal(t, int32(0), v, "they should be equal")
+	})
+}
+
+func TestToInt32WithDefault(t *testing.T) {
+	testData := []ToInt32WithDefaultTest{
+		{"TEST_INT32_WITH_DEFAULT_0", "0", 0, 0},
+		{"TEST_INT32_WITH_DEFAULT_2147483647", "2147483647", 2147483647, 2147483647},
+		{"TEST_INT32_WITH_DEFAULT_2147483648", "2147483648", 0, 0},
+		{"TEST_INT32_WITH_DEFAULT_-2147483648", "-2147483648", -2147483648, -2147483648},
+		{"TEST_INT32_WITH_DEFAULT_-2147483649", "-2147483649", 0, 0},
+		{"TEST_INT32_WITH_DEFAULT_NOTANUMBER", "notanumber", 0, 0},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v := envconv.ToInt32WithDefault(td.env, td.defaultValue)
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v := envconv.ToInt32WithDefault("TEST_NON_EXISTANT", 105)
+		assert.Equal(t, int32(105), v, "they should be equal")
+	})
+}
+
+// Tests for int64 type
+type ToInt64Test struct {
+	env         string
+	value       string
+	expected    int64
+	errExpected bool
+}
+
+type ToInt64WithDefaultTest struct {
+	env          string
+	value        string
+	expected     int64
+	defaultValue int64
+}
+
+func TestToInt64(t *testing.T) {
+	testData := []ToInt64Test{
+		{"TEST_INT64_0", "0", 0, false},
+		{"TEST_INT64_9223372036854775807", "9223372036854775807", 9223372036854775807, false},
+		{"TEST_INT64_9223372036854775808", "9223372036854775808", 0, true},
+		{"TEST_INT64_-9223372036854775808", "-9223372036854775808", -9223372036854775808, false},
+		{"TEST_INT64_-9223372036854775809", "-9223372036854775809", 0, true},
+		{"TEST_INT64_NOTANUMBER", "notanumber", 0, true},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v, err := envconv.ToInt64(td.env)
+			if td.errExpected {
+				assert.Error(t, err, "there should be an error")
+			} else {
+				assert.NoError(t, err, "there should be no error")
+			}
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v, err := envconv.ToInt64("TEST_NON_EXISTANT")
+		assert.Error(t, err, "there should be an error")
+		assert.Equal(t, int64(0), v, "they should be equal")
+	})
+}
+
+func TestToInt64WithDefault(t *testing.T) {
+	testData := []ToInt64WithDefaultTest{
+		{"TEST_INT64_WITH_DEFAULT_0", "0", 0, 0},
+		{"TEST_INT64_WITH_DEFAULT_9223372036854775807", "9223372036854775807", 9223372036854775807, 9223372036854775807},
+		{"TEST_INT64_WITH_DEFAULT_9223372036854775808", "9223372036854775808", 0, 0},
+		{"TEST_INT64_WITH_DEFAULT_-9223372036854775808", "-9223372036854775808", -9223372036854775808, -9223372036854775808},
+		{"TEST_INT64_WITH_DEFAULT_-9223372036854775809", "-9223372036854775809", 0, 0},
+		{"TEST_INT64_WITH_DEFAULT_NOTANUMBER", "notanumber", 0, 0},
+	}
+
+	for _, td := range testData {
+		t.Run(td.env, func(t *testing.T) {
+			os.Setenv(td.env, td.value)
+			v := envconv.ToInt64WithDefault(td.env, td.defaultValue)
+			assert.Equal(t, td.expected, v, "they should be equal")
+		})
+	}
+
+	t.Run("TEST_NON_EXISTANT does not exist", func(t *testing.T) {
+		v := envconv.ToInt64WithDefault("TEST_NON_EXISTANT", 105)
+		assert.Equal(t, int64(105), v, "they should be equal")
 	})
 }
