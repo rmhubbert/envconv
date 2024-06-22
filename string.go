f6684be5084fd1ad34c45ff572dac48650275937
Hubby <hubby@afterdigital.co.uk>
2024-06-22 20:23:06 +0100

    add functionality to convert to slice of strings

--- string.go
+++ string.go
@@ -1,5 +1,7 @@
 package envconv
 
+import "strings"
+
 // ToString returns the value of the requested environment variable
 // without converting from the original string. An error will be
 // returned if the environment variable is not found.
@@ -21,4 +23,22 @@ func ToString(varName string) (string, error) {
 // of pattern consistency.
 func ToStringWithDefault(varName string, defaultValue string) string {
 	return LoadFromEnvironmentWithDefault(varName, defaultValue)
+}
+
+func ToStringSlice(varName string, separator string) ([]string, error) {
+	value, err := LoadFromEnvironment(varName, true)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return strings.Split(value, separator), nil
+}
+
+func ToStringSliceWithDefault(varName string, separator string, defaultValue []string) []string {
+	value, err := ToStringSlice(varName, separator)
+	if err != nil {
+		return defaultValue
+	}
+
+	return value
 }
